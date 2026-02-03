bf428b1ab38c8df6cde4e0242a9a89f600a16298
carlotapachi24-gif <carlotapachi24@gmail.com>
2026-02-03 11:11:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-export default defineConfig({
-  base: "/loire-echo/",
-});
 
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/loire-echo/",
   server: {
     host: "::",
     port: 8080,
